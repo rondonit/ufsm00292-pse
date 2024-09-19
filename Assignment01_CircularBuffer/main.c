86790112f7fb5bb015343f8f6c2dbea185be8442
Gabriel Cruz <gabriel.cruz@ecomp.ufsm.br>
2024-09-19 00:44:09 -0300

    Addes insertBuffer function

--- Assignment01_CircularBuffer/main.c
+++ Assignment01_CircularBuffer/main.c
@@ -32,7 +32,30 @@ void createBuffer (BufferCircular* bc)
     bc->size  = 0;
 }
 
-static char * test_init_buffer (void)
+void insertBuffer (BufferCircular* bc, int value)
+{
+    /* This function inserts value in the end position */
+    /* Increments the end pointer and the size variable*/
+    /* It should check if the buffer is full */
+
+    *bc->end = value;
+    bc->end = bc->end + 1;
+    bc->size++;
+}
+
+int removeBuffer (BufferCircular* bc)
+{
+    /* This function removes the value pointed by start */
+    /* Increments start pointer to the next value */
+    /* Decrements the buffer size variable */
+
+    /* Gets the value to be removed */
+    int value = *bc->start;
+
+    return value;
+}
+
+static char * test_createBuffer (void)
 {
     /* Initializes the buffer */
     createBuffer(&bc);
@@ -49,10 +72,29 @@ static char * test_init_buffer (void)
     return 0;
 }
 
+static char * test_insertBuffer (void)
+{
+    /* Create buffer */
+    createBuffer(&bc);
+
+    /* Insert a value in buffer */
+    insertBuffer(&bc, 2);
+
+    /* Tests if the value was inserted */
+    assert("Erro: O valor não foi inserido na posição 0 do buffer.", bc.buffer[0] == 2);
+    
+    /* Tests if the end pointer was incremented */
+    assert("Erro: O valor do ponteiro end não foi incrementado.", bc.end == &bc.buffer[1]);
+
+    /* Tests if size variable was incremented */
+    assert("Erro: A variável size não foi incrementada.", bc.size == 1);
+}
+
 
 static char * all_tests(void)
 {
-    run_test(test_init_buffer);
+    run_test(test_createBuffer);
+    run_test(test_insertBuffer);
     return 0;
 }
 
